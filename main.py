cc356a5bfa165e5ed9446a0c1de37df5acb1b990
Your Name <your.email@example.com>
2024-12-04 19:05:22 +0330

    fix: empty and invalid operation

--- main.py
+++ main.py
@@ -6,34 +6,32 @@ from operations.divide import divide
 try:
     num_1 = int(input("\033[33m ----- Enter your first number: -----\033[30m\n"))
 except ValueError:
-    print("\033[33m *** Please enter your numbers ***\033[30m")
-    num_1 = ""
-    
+    raise SystemExit("\033[33m *** Please enter your numbers ***\033[30m")
+
 try:
     num_2 = int(input("\033[33m ----- Enter your second number: -----\033[30m\n"))
 except ValueError:
-    print("\033[33m *** Please enter your numbers ***\033[30m")
-    num_2 = ""
+    raise SystemExit("\033[33m *** Please enter your numbers ***\033[30m")
 
 op = input("\033[33m ----- Enter your operand: -----\033[30m\n")
 
-def main(n1, n2, o):
-    match o:
-        case "+":
-            return(add(n1, n2))
-        case "-":
-            return(subtract(n1, n2))
-        case "*":
-            return(multiply(n1, n2))
-        case "/":
-            return(divide(n1, n2))
-        case _:
-            return("Invalid operation")
-                
+if op == "":
+    raise SystemExit("\033[33m *** Please enter your operation ***\033[30m")
+else:
+
+    def main(n1, n2, o):
+        match o:
+            case "+":
+                return add(n1, n2)
+            case "-":
+                return subtract(n1, n2)
+            case "*":
+                return multiply(n1, n2)
+            case "/":
+                return divide(n1, n2)
+            case _:
+                return "Invalid operation"
+
+
 res = main(num_1, num_2, op)
 print("\033[32m ----- RESULT ----- \n\033[1m" + str(res) + "\033[30m\033[0m")
-
-
-# TODO 1. fix git ignore
-
-# TODO 3. handle edge cases
